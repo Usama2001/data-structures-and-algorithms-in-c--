f5a578bd165c62bab92c50a8c42346e382354a2b
Usama <2020ce11@student.uet.edu.pk>
2023-12-24 23:05:04 +0500

    Update doubly linked list file (11th Commit)

--- week one/Linked list/doublylinkedlist.cpp
+++ week one/Linked list/doublylinkedlist.cpp
@@ -1,2 +1,38 @@
 #include<iostream>
 
+
+struct node
+{
+    int data;
+    node *next;
+    node *previous;
+};
+
+class doublylinkedlist
+{
+private:
+struct node *head, *tail, *newnode;
+
+public:
+    doublylinkedlist(){
+        head=0;
+        newnode->next=0;
+        newnode->previous=0;
+    }
+
+    void add_node(){
+        node *newnode = new node;
+        std::cout << "Enter the element: ";
+        std::cin >> newnode->data;
+
+
+
+    }
+    
+
+};
+
+
+doublylinkedlist::~doublylinkedlist()
+{
+}
